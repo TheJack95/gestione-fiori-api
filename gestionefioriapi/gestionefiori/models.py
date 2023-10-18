cb6b189e149f6249200b7d9335f6010d62fa468e
Giacomo <giacomocorro@gmail.com>
2023-10-18 11:06:04 +0200

    First commit

---
+++ gestionefioriapi/gestionefiori/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+# Create your models here.
+class Item(models.Model):
+    art_number = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+    original_price = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+    price = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+    ean_code = models.CharField(max_length=50)
+    image_name = models.CharField(max_length=200, null=True)
+    image_url = models.CharField(max_length=200, null=True)
+
+    class Meta:
+        db_table = "items"
---
+++ gestionefioriapi/gestionefiori/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework.serializers import ModelSerializer
+
+from .models import Item
+
+class ItemSerializer(ModelSerializer):
+    class Meta:
+        model = Item
+        fields = ["id", "art_number", "description", "original_price", "price", "ean_code", "image_name", "image_url"]
